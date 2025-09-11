596e61f4ca5a6eb48cb63edb4c28077c3661372b
Ryan Loh <ryanloh28@yahoo.com.sg>
2025-09-11 13:11:41 +0800

    made changes to agent inputs
    agent can now take in policy file path and config file path to produce remediation changes

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 
 
 @tool
-def run_conftest(path: str) -> str:
-    """Runs a conftest against a file with the given path"""
-    result = subprocess.run(["conftest", "test", path], capture_output=True)
+def run_conftest(config_path: str, policy_path: str) -> str:
+    """Runs a conftest on a config file against a opa policy file"""
+    result = subprocess.run(["conftest", "test", config_path, "--policy", policy_path], capture_output=True)
     output = result.stdout.decode("utf-8")
+    print(output)
     # output += result.stderr.decode("utf-8"), not needed since tool throwing error is handled by langchain
 
     return output
@@ -17,12 +18,33 @@ def run_conftest(path: str) -> str:
 def main():
     load_dotenv()
     agent = create_react_agent(
-        model="openai:gpt-4.1-mini", tools=[run_conftest]
+        model="openai:gpt-4.1-mini",
+        tools=[run_conftest],
+        prompt="""
+            You are a helpful assistant that generates a recommended fix for a configuration with security vulnerabilities.
+            You are provided with the configuration file and the opa policy file directory that corresponds to this type of configuration.
+            You will use the run_conftest tool to determine what issues are present by passing in the configuration file path and the policy file path.
+        """,
     )
 
     input_message = {
         "role": "user",
-        "content": "what is the conftest output for the file in sample-terraform/ecr.tf?",
+        "content": """
+            This is the file content, which is located in sample-terraform/ecr.tf:
+
+            provider "aws" {
+              region = "ap-southeast-1"
+            }
+            resource "aws_ecr_repository" "scrooge_ecr" {
+              name                 = "scrooge-ecr"
+
+              image_scanning_configuration {
+                scan_on_push = true
+              }
+            }
+
+            What is the recommended changes for this file against the policy in policy/deny.rego?
+        """,
     }
     for step in agent.stream(
         {"messages": [input_message]}, stream_mode="values"
---
+++ policy/deny-v2.rego
@@ -0,0 +1,26 @@
+package main
+import rego.v1
+
+deny contains msg if {
+    some name
+    some repo in input.resource.aws_ecr_repository[name]
+    repo.image_tag_mutability == "MUTABLE"
+    msg = sprintf("ECR repository `%v` is not set to immutable", [name])
+}
+
+deny contains msg if {
+    some name
+    some repo in input.resource.aws_ecr_repository[name]
+    not repo.image_scanning_configuration
+    msg := sprintf("ECR repository '%v' should have image_scanning_configuration defined", [name])
+}
+
+deny contains msg if {
+    some name
+    some repo in input.resource.aws_ecr_repository[name]
+    repo.image_scanning_configuration
+    scan_config := repo.image_scanning_configuration[_]
+    not scan_config.scan_on_push
+    msg := sprintf("ECR repository '%v' should have scan_on_push enabled", [name])
+}
+
