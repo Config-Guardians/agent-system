c6a5e6e21a88c438aa81638e0771452075fa33c5
Ryan Loh <ryanloh28@yahoo.com.sg>
2025-10-01 17:21:03 +0800

    chore: zac complaining about redundant use of function wrap

--- main.py
+++ main.py
@@ -12,92 +12,53 @@ from agents.monitoring import monitoring_node
 from agents.remediation import remediation_node
 
 
-def main():
+workflow = StateGraph(MessagesState)
+workflow.add_node("monitoring", monitoring_node)
+workflow.add_node("remediation", remediation_node)
 
-    workflow = StateGraph(MessagesState)
-    workflow.add_node("monitoring", monitoring_node)
-    workflow.add_node("remediation", remediation_node)
+workflow.add_edge(START, "monitoring")
+graph = workflow.compile()
 
-    workflow.add_edge(START, "monitoring")
-    graph = workflow.compile()
+hachiware_endpoint = os.getenv('HACHIWARE_ENDPOINT')
+if not hachiware_endpoint:
+    raise ValueError("Missing HACHIWARE_ENDPOINT env var")
 
-    hachiware_endpoint = os.getenv('HACHIWARE_ENDPOINT')
-    if not hachiware_endpoint:
-        raise ValueError("Missing HACHIWARE_ENDPOINT env var")
+messages = SSEClient(f"{hachiware_endpoint}/sse")
 
-    messages = SSEClient(f"{hachiware_endpoint}/sse")
+print("Waiting for hachiware...")
+try:
+    for msg in messages:
+        if msg.data:
+            data = json.loads(msg.data)
+            file = data["data"]
+            filename = file['path'].split("/")[-1]
+            print(file['path'])
+            if not file['path'] == 'infra/modules/storage/main.tf':
+                continue
 
-    try:
-        for msg in messages:
-            if msg.data:
-                data = json.loads(msg.data)
-                file = data["data"]
-                filename = file['path'].split("/")[-1]
-                print(file['path'])
-                if not file['path'] == 'infra/modules/storage/main.tf':
-                    continue
+            if not os.path.isdir("tmp"):
+                os.mkdir("tmp")
+            with open(f"tmp/{filename}", "w") as f: # TODO: security vulnerability
+                f.write(file['content'])
 
-                if not os.path.isdir("tmp"):
-                    os.mkdir("tmp")
-                with open(f"tmp/{filename}", "w") as f: # TODO: security vulnerability
-                    f.write(file['content'])
+            prompt = "This is the file content:\n"
+            prompt += file["content"]
+            prompt += f"What are the recommended changes for this file \"{filename}\" against the policy in policy/deny-s3.rego?"
 
-                prompt = "This is the file content:\n"
-                prompt += file["content"]
-                prompt += f"What are the recommended changes for this file \"{filename}\" against the policy in policy/deny-s3.rego?"
+            message = HumanMessage(prompt)
+            msg_state = MessagesState(messages=[message])
+            events = graph.stream(msg_state,
+                {"recursion_limit": 20},
+                stream_mode='values'
+            )
+            final_state = None
+            for s in events:
+                print(s["messages"][-1].pretty_print())
+                print("----")
+                final_state = s
 
-                message = HumanMessage(prompt)
-                msg_state = MessagesState(messages=[message])
-                events = graph.stream(msg_state,
-                    {"recursion_limit": 20},
-                    stream_mode='values'
-                )
-                final_state = None
-                for s in events:
-                    print(s["messages"][-1].pretty_print())
-                    print("----")
-                    final_state = s
-
-                if final_state:
-                    generate_report(final_state["messages"])
-    except KeyboardInterrupt:
-        print("Interrupt detected, terminating gracefully")
-        messages.resp.close()
-
-    # COMMENTED OUT: Sample terraform usage (uncomment when needed)
-    # sample_file = "sample-terraform/ecr.tf"
-    # filename = os.path.basename(sample_file)
-    
-    # with open(sample_file, "r") as f:
-    #     content = f.read()
-    
-    # if not os.path.isdir("tmp"):
-    #     os.mkdir("tmp")
-    # with open(f"tmp/{filename}", "w") as f:
-    #     f.write(content)
-    
-    # prompt = "This is the file content:\n"
-    # prompt += content
-    # prompt += f"What are the recommended changes for this file \"{filename}\" against the policy in policy/deny.rego?"
-    
-    # input_message = {
-    #     "role": "user",
-    #     "content": prompt,
-    # }
-    
-    # print(f"Starting policy compliance workflow for {filename}...")
-    # print("=" * 60)
-    
-    # events = graph.stream(
-    #     {
-    #         "messages": [input_message],
-    #     },
-    #     {"recursion_limit": 150},
-    #     stream_mode='values'
-    # )
-    
-    # for s in events:
-    #     print(s["messages"][-1].pretty_print())
-    #     print("----")
-    
-main()
+            if final_state:
+                generate_report(final_state["messages"])
+except KeyboardInterrupt:
+    print("Interrupt detected, terminating gracefully")
+    messages.resp.close()
