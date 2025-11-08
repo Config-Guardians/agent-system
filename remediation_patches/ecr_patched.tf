8ec2aa1392a46bd8afaf9db3b4febd95c7c39671
kylie-kiaying <kylie.kiayingchua@gmail.com>
2025-11-08 23:02:28 +0800

    Automated Remediation Patch

---
+++ remediation_patches/ecr_patched.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+resource "aws_ecr_repository" "scrooge_ecr" {
+  name                 = "scrooge-ecr"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability = "IMMUTABLE"
+}
