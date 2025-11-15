c41ec6dcf5eb377224f204b4a7e926c411579b5b
Ryan Loh <ryanloh28@yahoo.com.sg>
2025-11-15 21:25:06 +0800

    Automated Remediation Patch

--- sample-configs/ecr.tf
+++ sample-configs/ecr.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
 resource "aws_ecr_repository" "scrooge_ecr" {
   name                 = "scrooge-ecr"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
